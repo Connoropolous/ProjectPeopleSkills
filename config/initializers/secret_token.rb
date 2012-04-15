674c615d52ff01189a13676e81cfd28017490643
Connor T <connorturland@gmail.com>
2012-04-15 17:54:07 -0400

    added all RoR app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectPeopleSkills::Application.config.secret_token = '6f73cb7dee243c460921decccc9af7f3cf4da8cef4e3589f3c6a53012d13a4952a0d406d2354ddaa9ad2198b6a66d22fb22861599aef454f072aeaec6a8699d6'
